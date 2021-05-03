0137b6b9068ac6b2302a574961c3f1a8d8cdc8f3
Paulo Assis <pauloassisjunior@hotmail.com>
2021-05-03 03:07:19 -0300

    'commit'

--- src/app.js
+++ src/app.js
@@ -39,7 +39,7 @@ class App {
   middlewares() {
     this.app.use(cors(corsOptions));
     this.app.use(helmet());
-    this.app.use(delay(2000));
+    //this.app.use(delay(2000));
     this.app.use(express.urlencoded({ extended: true }));
     this.app.use(express.json());
     this.app.use('/images/', express.static(resolve(__dirname, '..', 'uploads', 'images')));
